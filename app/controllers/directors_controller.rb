03456e21cd649073cd86a93f00c80b99289aa809
osha7 <ogroetz@gmail.com>
2022-05-01 13:51:42 +0000

    youngest/eldest

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,10 +6,27 @@ class DirectorsController < ApplicationController
   end
 
   def youngest
+    @list_of_directors = Director.all
+    @directors = Director.order('dob ASC')
+    
+    @youngest = @directors[-1]
+    @youngest_name = @youngest.name
+    @youngest_date = (@youngest.dob).strftime("%b %e, %Y")
+
     render ({ :template => "director_templates/youngest.html.erb"})
   end
 
   def eldest
+    @list_of_directors = Director.all
+    @directors = Director.order('dob ASC')
+
+    x = 0
+    while @directors[x].dob == nil 
+      x += 1
+    end
+    @eldest = @directors[x]
+    @eldest_name = @eldest.name
+    @eldest_date = (@eldest.dob).strftime("%b %e, %Y")
     render ({ :template => "director_templates/eldest.html.erb"})
   end
 
